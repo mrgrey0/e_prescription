49852edd2495937488538f25965d66850a87da7a
mrgrey0 <108786019+mrgrey0@users.noreply.github.com>
2024-02-04 13:20:10 +0530

    pending user reg logic

---
+++ E-Precription/auth_methods.py
@@ -0,0 +1,32 @@
+import pyrebase
+
+firebaseConfig = {
+  'apiKey': "AIzaSyDqoRHH1cbTWLUYRlH1vP5R-zrkCCAT0MI",
+  'authDomain': "e-prescription-ff041.firebaseapp.com",
+  'projectId': "e-prescription-ff041",
+  'storageBucket': "e-prescription-ff041.appspot.com",
+  'messagingSenderId': "437020753136",
+  'appId': "1:437020753136:web:c2c432dd239a9debfe1635",
+  'databaseURL':''
+}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+
+def login(email,password):
+    result = ''
+    try:
+        auth.sign_in_with_email_and_password(email,password)
+        result = 'success'
+    except :
+        result = 'unsuccess'
+    return result
+
+def signUp(mail,passW):
+        result = ''
+        try:
+            login = auth.create_user_with_email_and_password(mail,passW)
+            result ='success'
+        except :
+            result = 'unsuccess'
+        return result
--- E-Precription/main2.py
+++ E-Precription/main2.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template,request, url_for
+import auth_methods
 
 app = Flask(__name__)
 
@@ -10,6 +11,23 @@ def register():
 def index():
     return render_template("index.html")
 
+@app.route('/homepage')
+def homepage():
+    return "WELCOME TO HOME PAGE"
+
+@app.route('/loginChck',methods=['POST','GET'])
+# pending user creating logic 
+def checklogin():
+    if request.method=='POST':
+        username = request.form.get['uname']
+        password = request.form.get['pass']
+        res = auth_methods.signUp(username,password)
+        if res=='success':
+            return redirect(url_for('/homepage'))
+        elif res == 'unsuccess':
+            return "<P>Something went wrong</P>"
+    else :
+        return "NONE"
 
 if __name__ == '__main__':
     app.run(debug=True)
