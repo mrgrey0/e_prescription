99f9bde823a621f358d451847afb8c98ad5fcace
mrgrey0 <108786019+mrgrey0@users.noreply.github.com>
2024-02-11 21:48:02 +0530

    sql progress #2

--- E-Precription/sqlmethods.py
+++ E-Precription/sqlmethods.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="sql.freeasphost.net\MSSQL2016",
-  user="not0p",
-  password="@Rose1"
+  host='sql6.freesqldatabase.com',
+  user="sql6682428",
+  password="x1uSP9bZTb",
+  database='sql6682428'
 )
 def addUser():
     pass
@@ -11,4 +12,15 @@ def addUser():
 def getUser():
     pass 
 # WORK IS PENDINGGGGGGGGGG#sdasdasddasdasd
+# need to create a function that takes values and inserts it to sql database using a query.
+# then call it to register user route to store database of newly created user in sql database.
 print(mydb)
+cursorObject = mydb.cursor()
+if __name__ =='__main__':
+    ##cursorObject.execute("""CREATE TABLE USERS (
+#                   Username  VARCHAR(200) NOT NULL,
+#                   Email VARCHAR(50),
+#                   phNumber INT,
+#                   Password VARCHAR(200),
+#                   IsDoc BOOL
+#                   )""")
