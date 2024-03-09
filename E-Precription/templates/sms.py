40a7db700d83030f951af484dfe62459e027465d
mrgrey0 <108786019+mrgrey0@users.noreply.github.com>
2024-03-09 20:45:02 +0530

    pdf 1st commit

--- E-Precription/templates/sms.py
+++ E-Precription/templates/sms.py
@@ -1,31 +1,14 @@
 
-import os
+from twilio.rest import Client
 
-from telesign.messaging import MessagingClient
+client = Client('AC21c1c76dcadf024fa600e9eaec2bb166', 'f14c815d0c085bc635cba13342751abe')
 
 
-# Replace the defaults below with your Telesign authentication credentials or pull them from environment variables.
-customer_id = '80A36A19-25FF-4D9C-B9D9-D4240CD84DE7'
-api_key = 'GHGsyVXUduEppKiM6iGv3jA0gn495+WgiB33NzNiUdFXpxSvR7TiD+sH33ClxCuQXWyRBMHB3hAipaRNDWvCug=='
-
-# Set the default below to your test phone number or pull it from an environment variable. 
-# In your production code, update the phone number dynamically for each transaction.
-phone_number = '917350314101'
-
-# Set the message text and type.
-message = "SMS service is working"
-message_type = "ARN"
-
-# Instantiate a messaging client object.
-messaging = MessagingClient(customer_id, api_key)
-
-# Make the request and capture the response.
-#response = messaging.message(phone_number, message, message_type)
 def sendMessage(num,text):
-    response = messaging.message(num, text, message_type)
-    return response.body
+    message = client.messages \
+                .create(
+                     body=text,
+                     from_='+18646063522',
+                     to=num
+                 )
 
-# Display the response body in the console for debugging purposes. 
-# In your production code, you would likely remove this.
-if __name__ == '__main__':
-    print(sendMessage('917350314101','its working'))
